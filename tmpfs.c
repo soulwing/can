43f50dddf75c3548557fa7432a4aa90b881d66a7
Carl Harris <ceharris@vt.edu>
2017-09-27 16:27:02 -0400

    add tmpfs mount support

---
+++ tmpfs.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017 Carl Harris Jr, Michael Irwin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <sys/mount.h>
+
+#include "dstring.h"
+#include "config.h"
+#include "tmpfs.h"
+
+#define TMPFS_TYPE  "tmpfs"
+
+int mount_proc(const char *root_path, const char *path, size_t size)
+{
+  String *mount_path = dstr_init(root_path);
+  dstr_append(mount_path, path);
+
+  int rc = mount(TMPFS_TYPE, dstr_text(mount_path), TMPFS_TYPE, MS_NOEXEC, MS_NOSUID, 
+      "size=101468k,mode=755");
+
+  dstr_free(mount_path);
+  return rc;
+}
